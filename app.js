3633d4c816302a6bf36738760da8a29a4d6ff657
Ahmed Melouk <forprogramming8@gmail.com>
2022-07-21 12:05:24 +0200

    Generate divs with grid

--- app.js
+++ app.js
@@ -19,6 +19,24 @@ function generateDivs(num) {
 	}
 }
 
+function generateDivsWithGrid(num) {
+	let containerDiv = document.querySelector("body > div");
+	for (let i = 0; i < num * num; i++) {
+		let cell = document.createElement('div')
+		cell.className = 'cell'
+		// cell.innerHTML = i;
+		// cell.style.backgroundColor = 'blue';
+		cell.addEventListener('mouseover', () => {
+			cell.classList.add('hoverd')
+			cell.style.backgroundColor = 'red';
+		})
+		containerDiv.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+		containerDiv.style.gridTemplateRow = `repeat(${num}, 1fr)`;
+		containerDiv.appendChild(cell);
+
+	}
+}
+
 
 
 
