1a14367002ce4ad500ccf442696d4cba682104a9
Ahmed Melouk <forprogramming8@gmail.com>
2022-07-21 20:12:17 +0200

    Enhance generateDivsWithGrid function
    clear out any exsisting cell

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 function generateDivsWithGrid(num) {
 	let containerDiv = document.querySelector("body > div");
+
+	// clear out any exsisting cell
+	let cells = containerDiv.querySelectorAll('div')
+	cells.forEach(cell => cell.remove)
+
 	for (let i = 0; i < num * num; i++) {
 		let cell = document.createElement('div')
-		// cell.className = 'cell'
-		cell.innerHTML = i;
+		cell.className = 'cell'
+		// cell.innerHTML = i;
 		// cell.style.backgroundColor = 'blue';
 		cell.addEventListener('mouseover', () => {
 			// cell.classList.add('hoverd')
